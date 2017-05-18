26762842937b288b776efb547556d6a2fc61fbc1
Herry Xing <494989582@qq.com>
2017-05-18 14:25:36 +0800

    Update Url.java

--- app/src/main/java/com/zhuancheng/sanhedefence/domain/Url.java
+++ app/src/main/java/com/zhuancheng/sanhedefence/domain/Url.java
@@ -6,5 +6,5 @@ package com.zhuancheng.sanhedefence.domain;
 public interface Url {
     String DOMAIN = "http://j171839l97.iask.in:27592/";
     String PATH = "zcccadw/mobile/";
-
+    String PATH1 = "zcccadw/mobile/";
 }
